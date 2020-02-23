20d47f4aa9999b11419e0c54830f0f322061b0c6
Ryan Clark <ryanjclark91@gmail.com>
2020-02-23 09:59:39 -0600

    added dataset

--- AmesHousingModeling.r
+++ AmesHousingModeling.r
@@ -4,3 +4,24 @@
 
 ## Objective: Perform detailed analysis on Ames Housing Data
 ## to demonstrate ...
+
+###
+# Load libraries
+###
+library(ggplot2)
+library(corrplot)
+library(plyr)
+library(GGally)
+library(lattice)
+library(moments)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(gridExtra)
+library(kableExtra)
+library(datasets)
+library(forcats)
+library(caret)
+library(lessR)
+library(calibrate)
+options(scipen=1000)
